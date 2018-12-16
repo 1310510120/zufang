380e898f888318a0dad25b369601f2481077f968
faith <1310510120@qq.com>
2018-12-16 16:03:14 +0800

    新增house_cat表，

---
+++ src/main/java/com/zufang/pojo/House_cat.java
@@ -0,0 +1,24 @@
+package com.zufang.pojo;
+
+public class House_cat {
+
+    private Integer id;
+
+    private String cat_name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCat_name() {
+        return cat_name;
+    }
+
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
+    }
+}
